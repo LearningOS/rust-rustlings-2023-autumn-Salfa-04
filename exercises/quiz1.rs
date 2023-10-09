86c9eea55f2696f318bef664b428fc6a19b5cd9d
Salfa Chang <salfa@foxmail.com>
2023-10-09 21:50:25 +0800

    first commit: 43percent!

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
+
+fn calculate_price_of_apples(nums: u8) -> u8 {
+    if nums > 40 {
+        nums
+    } else {
+        2 * nums
+    }
+}
 
 // Don't modify this function!
 #[test]
